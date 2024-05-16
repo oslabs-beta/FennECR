8641eec66e3e891a61a013fc0b4edbdf19bd7453
Jing Xia <xiajing1103@gmail.com>
2024-05-16 13:42:21 -0400

    add the header in container

--- client/insightecr/src/App.tsx
+++ client/insightecr/src/App.tsx
@@ -1,14 +1,46 @@
-import "./App.css";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
 import PosNegBarChart from './Components/BarChart';
+import BasicPie from './Components/PieChart';
+import NavBar from './Components/NavBar';
+import BasicContainer from './Components/Container';
 import Header from './Components/Header';
-import NavDrawer from './Components/NavigationDrawer'
-import NavBar from './Components/NavBar'
 
-function App() {
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function BasicGrid() {
   return (
-   <div></div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Item>
+            <NavBar/>
+          </Item>
+        </Grid>
+        <Grid item xs={5}>
+          <Item>
+            <BasicContainer header={Header} title='PieChart' component={BasicPie}/>
+          </Item>
+        </Grid>
+        <Grid item xs={7}>
+          <Item>
+            <BasicContainer header={Header} title='BarChart' component={PosNegBarChart}/>
+          </Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
-
-export default App;
--- client/insightecr/src/Components/Container.tsx
+++ client/insightecr/src/Components/Container.tsx
@@ -3,15 +3,27 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
-export default function BasicContainer({component: Component}){
+
+
+interface BasicContainerProps {
+    header: React.ComponentType<{ title: string }>;
+    title: string; 
+    component: React.ComponentType<{}>;
+}
+
+// export default function BasicContainer({component: Component}){
+const BasicContainer: React.FC<BasicContainerProps> = ({header: Header, title, component:Component}) => {
     return (
         <React.Fragment>
             <CssBaseline />
             <Container maxWidth = 'sm'>
                 <Box sx={{bgcolor: '#E8E8FD'}}>
+                    <Header title = {title} />
                     <Component />
                 </Box>
             </Container>
         </React.Fragment>
     );
 }
+
+export default BasicContainer;
