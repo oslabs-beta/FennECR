4036072b7b507e5f6e1d18297282e7bb0871aad1
Richard Araujo <rcad14@gmail.com>
2024-05-16 12:34:25 -0400

    Fixed Detail Card spacing

--- client/insightecr/src/App.tsx
+++ client/insightecr/src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import VulnerabilityDetails from './Components/VulnerabilityDetails.tsx'
 
 function App() {
--- client/insightecr/src/Components/DetailsCard.tsx
+++ client/insightecr/src/Components/DetailsCard.tsx
@@ -1,14 +1,14 @@
 import { Paper } from '@mui/material';
-import '../App.css'
+import '../App.scss'
 import { Grid } from '@mui/material';
 
 const DetailsCard = () => {
     return (
-    <Grid container spacing={20}>
+    <Grid container spacing={28}>
     <Grid item>
         <Paper id='detailsCard1' elevation={2}>sample-Repo 1</Paper>
     </Grid>
-    <Grid item>
+    <Grid item >
         <Paper id='detailsCard2' elevation={2}>sample-repo 2</Paper>
     </Grid>
   </Grid>    
