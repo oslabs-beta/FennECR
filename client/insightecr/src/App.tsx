2827313c14ac84731d6ad7030b7ab3d9d64dbb14
Jing Xia <xiajing1103@gmail.com>
2024-05-16 11:25:20 -0400

    pieChart and container

--- client/insightecr/src/App.tsx
+++ client/insightecr/src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import React from "react";
+import { ReactDOM } from "react";
+import BasicPie from "./Components/PieChart";
+import BasicContainer from "./Components/Container";
+
 
 function App() {
 
   return (
     <>
-      <h1>InsightECR</h1>
+      {/* <h1>InsightECR</h1> */}
+        <BasicContainer component={BasicPie} />
+
     </>
   );
 }
