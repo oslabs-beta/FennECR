237ece1ab31c12b479a408c5c846f65a7af95f44
Peter Gao <peter_gao@outlook.com>
2024-06-04 08:18:18 -0700

    get stuck

--- server/dist/models/dynamoDB.js
+++ server/dist/models/dynamoDB.js
@@ -11,8 +11,8 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 // Configure the DynamoDB client to point to the local DynamoDB instance
 const ddbClient = new client_dynamodb_1.DynamoDBClient({
-    region: process.env.AWS_REGION_1 || 'us-east-1',
-    endpoint: 'http://dynamodb:8000', // Point to local DynamoDB
+    region: process.env.AWS_REGION_1 || "us-east-1",
+    endpoint: "http://localhost:8000", // Point to local DynamoDB
     credentials: (0, credential_provider_env_1.fromEnv)(),
 });
 const ddbDocClient = lib_dynamodb_1.DynamoDBDocumentClient.from(ddbClient);
--- server/src/models/dynamoDB.ts
+++ server/src/models/dynamoDB.ts
@@ -1,19 +1,18 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { fromEnv } from "@aws-sdk/credential-provider-env";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Configure the DynamoDB client to point to the local DynamoDB instance
 const ddbClient = new DynamoDBClient({
-  region: process.env.AWS_REGION_1 || 'us-east-1', 
-  endpoint: 'http://dynamodb:8000', // Point to local DynamoDB
-  credentials: fromEnv(), 
+  region: process.env.AWS_REGION_1 || "us-east-1",
+  endpoint: "http://localhost:8000", // Point to local DynamoDB
+  credentials: fromEnv(),
 });
 
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
 
 export default ddbDocClient;
-
