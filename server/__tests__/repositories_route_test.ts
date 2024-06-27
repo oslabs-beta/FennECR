153e5646902109feed08dad5b95609d29ba2a664
Cyane L <164072707+CyaneL@users.noreply.github.com>
2024-06-27 17:15:43 -0400

    1. Changed accountId from '1' to 'DEV' 2. Improved scanResults_route_test coverage to 100% (added testing for getting sigle scan result)

--- server/__tests__/repositories_route_test.ts
+++ server/__tests__/repositories_route_test.ts
@@ -1,34 +1,30 @@
-import request from "supertest";
-import server from "../src/index";
+import request from 'supertest';
+import server from '../src/index';
 
-
-describe("Repositories Routes", () => {
+describe('Repositories Routes', () => {
   // After running the tests, close the server
-    afterAll((done) => {
-      server.close(done);
-    });
-  
-    it("GET /:accountId/:repoName should get single repository data with name", async () => {
-      const res = await request(server).get('/repository/1/testrepo');
-      expect(res.status).toBe(200);
-      expect(Array.isArray(res.body.repositories)).toBe(true);
-      expect(res.body.repositories[0].repositoryName).toBe('testrepo');
-    });
-
-
-    it('GET /:accountId should return all repositories data', async () => {
-      const res = await request(server).get('/repository/1');
-      expect(res.status).toBe(200);
-      expect(Array.isArray(res.body.repositories)).toBe(true);
-      expect(res.body.repositories.length).toBeGreaterThan(0);
-    });
-
-
-    it('POST /:accountId/:repoName/scan-on-push should toggle scan on push setting', async () => {
-      const res = await request(server).post('/repository/1/testrepo/scan-on-push');
-      expect (res.status).toBe(200);
-    });
-
-
+  afterAll((done) => {
+    server.close(done);
   });
-  
+
+  it('GET /:accountId/:repoName should get single repository data with name', async () => {
+    const res = await request(server).get('/repository/DEV/testrepo');
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body.repositories)).toBe(true);
+    expect(res.body.repositories[0].repositoryName).toBe('testrepo');
+  });
+
+  it('GET /:accountId should return all repositories data', async () => {
+    const res = await request(server).get('/repository/DEV');
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body.repositories)).toBe(true);
+    expect(res.body.repositories.length).toBeGreaterThan(0);
+  });
+
+  it('POST /:accountId/:repoName/scan-on-push should toggle scan on push setting', async () => {
+    const res = await request(server).post(
+      '/repository/DEV/testrepo/scan-on-push'
+    );
+    expect(res.status).toBe(200);
+  });
+});
